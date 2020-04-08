0bbf9d4ab147f08f0be59097c3fb2cce84ad6bd9
AlessandroMondelli <alessandro.mon98@gmail.com>
2020-04-08 19:03:44 +0200

    Aggiunto log danni inflitti da player e monster, ottimizzate funzioni e codice

--- app.js
+++ app.js
@@ -4,66 +4,71 @@ new Vue({
         start: false,
         player: {
             hp: 100,
-            lastDamage: [],
         },
         monster: {
             hp: 100,
-            lastDamage: [],
         },
+        damages: {
+            playerLastDamage: [],
+            monsterLastDamage: [] 
+        }
     },
     methods: {
-        playerAttack: function() {
-            playerDamage = Math.floor(Math.random() * (10 - 1 + 1)) + 1; //Danno che può infliggere il giocatore (da 1 a 10)
-            
-            return playerDamage;
-        },
-        monsterAttack: function() {
-            monsterDamage = Math.floor(Math.random() * (10 - 1 + 1)) + 1; //Danno che può infliggere il mostro (da 1 a 10)
-            
-            return monsterDamage;
-        },
         doAttack: function() {
-            this.monster.hp -= this.playerAttack(); //Diminuisco vita del mostro
-            this.player.hp -= this.monsterAttack(); //Diminuisco vita del player
-            console.log(this.playerAttack());
-            console.log(this.monsterAttack());
+            var playerDamage = this.calcAttack(3,10); //Genero danno player
+            var monsterDamage = this.calcAttack(5,12); //Genero danno monster
+
+            this.monster.hp -= playerDamage; //Diminuisco vita del mostro
+            this.player.hp -= monsterDamage; //Diminuisco vita del player
             
-            this.monster.lastDamage.push(this.playerAttack());
-            this.player.lastDamage.push(this.monsterAttack());
+            this.damages.monsterLastDamage.push(monsterDamage); //Inserisco i danni in un array
+            this.damages.playerLastDamage.push(playerDamage); //Inserisco i danni in un array
+
+            this.winCheck(); //Richiamo funzione per verificare Vittoria/sconfitta
         },
         doSpecialAttack: function() {
-            this.monster.hp -= 10; //Diminuisco vita del mostro di 10
-            this.player.hp -= this.monsterAttack(); //Diminuisco vita del player
+            var playerDamage = this.calcAttack(12,18); //Genero danno player
+            var monsterDamage = this.calcAttack(5,12); //Genero danno monster
+            
+            this.monster.hp -= playerDamage; //Diminuisco vita del mostro
+            this.player.hp -= monsterDamage; //Diminuisco vita del player
 
-            this.monster.lastDamage.push(10);
-            this.player.lastDamage.push(this.monsterAttack());
+            this.damages.monsterLastDamage.push(monsterDamage); //Inserisco i danni in un array
+            this.damages.playerLastDamage.push(playerDamage); //Inserisco i danni in un array
+
+            this.winCheck(); //Richiamo funzione per verificare Vittoria/sconfitta
         },
         doHeal: function() {
-            if(this.player.hp < 100 && this.player.hp > 90) {
-                flag = 0 //Flag per verificare se il player è arrivato a 100 di vita
-                i = 0; //Contatore per while
-                do {
-                    i++; //Incremento contatore
-                    if(this.player.hp + i == 100) { //Se il contatore incrementato porta la salute a 100
-                        this.player.hp += i; //Aumento vita con il contatore
-                        flag = 1; //Salute aumentata!
-                    }
-                } while(flag == 0); 
-            } else if(this.player.hp <= 90) {
+            if(this.player.hp <= 90) { //Se il player ha -= 90hp
                 this.player.hp += 10; //Aumento vita del player di 10
+            } else { //Se il player ha più di 90hp
+                this.player.hp = 100; //Aumento vita del player fino a 100
             } 
-            else {
-                alert("You have 100HP!")
-            }
 
-            this.player.hp -= this.monsterAttack(); //Diminuisco vita del player
-            this.monster.lastDamage.push(monsterDamage);
+            var monsterDamage = this.calcAttack(5,12);
+            this.player.hp -= monsterDamage; //Diminuisco vita del player
+            this.damages.monsterLastDamage.push(monsterDamage); //Inserisco i danni in un array
+        },
+        calcAttack: function(min, max) {
+            damage = Math.floor(Math.random() * (max - min + 1)) + min; //Danno inflitto
+            
+            return damage;
+        },
+        winCheck: function() {
+            if(this.player.hp <= 0) {
+                alert("You're Dead!");
+                this.giveUp();
+            } else if(this.monster.hp <= 0) {
+                alert("You Won!");
+                this.giveUp();
+            }
         },
         giveUp: function(){
             this.player.hp = 100;
             this.monster.hp = 100;
+            this.damages.playerLastDamage = [];
+            this.damages.monsterLastDamage = [];
             this.start = false;
-            this. action = false;
         }
     },
 });
